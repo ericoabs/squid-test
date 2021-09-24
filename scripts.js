15f579eed186401489fef0394a2954f4459b293f
Erico Alexandre <ericoabs25@gmail.com>
2021-09-24 01:20:52 -0300

    Estilizando o hover

--- scripts.js
+++ scripts.js
@@ -23,18 +23,29 @@ document.addEventListener("DOMContentLoaded", function () {
       })
       .then((data) => {
         data.map((item) => {
-          // const col = document.createElement('div');
-          // col.setAttribute('class', 'col');
-
-          // row.appendChild(col);
-
           const image = document.createElement('img');
-          image.setAttribute('src', item.imagens.resolucaoMedia.url);
+          image.setAttribute('src', item.imagens.thumbnail.url);
+          
+          const overlay = document.createElement('div');
+          overlay.setAttribute('class', 'overlay');
+          
+          const userName = document.createElement('div');
+          userName.setAttribute('class', 'text');
+          userName.innerHTML += '@' + item.usuario.username;
 
+          overlay.appendChild(userName);
+
+          const upvotes = document.createElement('div');
+          upvotes.setAttribute('class', 'text');
+          upvotes.innerHTML += '@' + item.usuario.username;
+          
+          
           const anchor = document.createElement('a');
           anchor.setAttribute('href', item.link);
+          anchor.setAttribute('class', 'overlayContainer');
 
           anchor.appendChild(image);
+          anchor.appendChild(overlay);
 
           const col = document.createElement('div');
           col.setAttribute('class', 'col');
